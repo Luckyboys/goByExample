59d7724d32913d572ff7e82fa0d4acc0df157924
Luckyboys <eacholwong@gmail.com>
2019-12-03 11:09:18 +0800

    feat(json): 添加了测试JSON编码失败的尝试代码

--- json.go
+++ json.go
@@ -1,32 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin/json"
 	"time"
 )
 
 func main() {
 
+	testDurationJSON()
+	testJSONMarshalError()
+}
+
+func testJSONMarshalError() {
+	data, err := json.Marshal(struct {
+		A interface{}
+	}{make(chan int)})
+	fmt.Printf("data: %v, err: %v", string(data), err)
+}
+
+func testDurationJSON() {
 	duration, err := time.ParseDuration("1h")
 	if err != nil {
 
 		fmt.Println(err)
 		return
 	}
-
 	data := struct {
 		Duration time.Duration
 	}{
 		Duration: duration,
 	}
 	byteData, err := json.Marshal(data)
-
 	if err != nil {
 
 		fmt.Println(err)
 		return
 	}
-
 	fmt.Println(string(byteData))
 }
